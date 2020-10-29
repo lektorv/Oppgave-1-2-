3406c65a7db2f97581bc6b98086b29702c13cc9a
lektorv <martine.vold@hammerfest.kommune.no>
2020-10-29 12:56:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Oppgave 1
+
+# Jeg skal skrive ut en liste med antall personer i hver hobby. Fiks koden slik at den funker, og fullfør koden.
+
+fotball = 15
+håndball: 14
+klatring = 3
+kor = fem
+
+print ("Det går", fotball, "på fotball")
+
+
+# Oppgave 2
+
+# Fiks koden
+
+print ("Mitt navn er", navn)
+print ("Jeg er 14")
+print (Jeg har, søsken, "søsken")
+
+navn = Martine
+14 år
+søsken = 7
